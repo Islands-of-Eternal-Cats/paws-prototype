958ae0ab8d3d561656f61b196f78bd8eb0b2227a
mrstreamer963 <698lo7epy@mozmail.com>
2026-05-23 07:42:38 +0300

    feat: add ActionButtons component

---
+++ squad-panel/src/components/ActionButtons.tsx
@@ -0,0 +1,12 @@
+import './ActionButtons.css';
+
+function ActionButtons() {
+  return (
+    <div className="action-buttons">
+      <button className="action-btn primary">RESUPPLY TO TEMPLATE</button>
+      <button className="action-btn">AUTO ✓</button>
+    </div>
+  );
+}
+
+export default ActionButtons;
