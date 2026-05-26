a8c986e899041e8a6c43ebb59b1ca8340dfec03d
sir Random <random@amberskynet.org>
2026-05-26 23:00:33 +0300

    update PhaseBanner

--- packages/ui/src/components/PhaseBanner.tsx
+++ packages/ui/src/components/PhaseBanner.tsx
@@ -21,27 +21,27 @@ export function PhaseBanner({ state }: Props) {
     const atBase = state.squads.find((s) => s.phase === 'AtBase')
     if (!atBase) return null
     const sec = Math.ceil(atBase.phaseTimeLeftMs / 1000)
-    return <div className="phase-banner">{PHASE_LABELS.AtBase} {sec}s</div>
+    // return <div className="phase-banner">{PHASE_LABELS.AtBase} {sec}s</div>
   }
 
-  return (
-    <div className="phase-banner">
-      {activeSquads.map((s) => {
-        const label = PHASE_LABELS[s.phase] || s.phase
-        if (s.phase === 'AtBase') {
-          const sec = Math.ceil(s.phaseTimeLeftMs / 1000)
-          return (
-            <span key={s.id}>
-              [{s.id}] {label} {sec}s
-            </span>
-          )
-        }
-        return <span key={s.id}>[{s.id}] {label}</span>
-      }).reduce((prev, curr, i, arr) => i === 0 ? curr : (
-        <span>
-          {prev} | {curr}
-        </span>
-      ))}
-    </div>
-  )
+  // return (
+  //   <div className="phase-banner">
+  //     {activeSquads.map((s) => {
+  //       const label = PHASE_LABELS[s.phase] || s.phase
+  //       if (s.phase === 'AtBase') {
+  //         const sec = Math.ceil(s.phaseTimeLeftMs / 1000)
+  //         return (
+  //           <span key={s.id}>
+  //             [{s.id}] {label} {sec}s
+  //           </span>
+  //         )
+  //       }
+  //       return <span key={s.id}>[{s.id}] {label}</span>
+  //     }).reduce((prev, curr, i, arr) => i === 0 ? curr : (
+  //       <span>
+  //         {prev} | {curr}
+  //       </span>
+  //     ))}
+  //   </div>
+  // )
 }
